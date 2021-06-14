8e9bf47bb1f694d1895533f765d48152b83ccd99
David Lindemann <34068491+dlindem@users.noreply.github.com>
2021-06-14 22:55:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 datafolder = "D:/Ahotsak/"
 
 awbuser = "DavidL_bot"
+with open(datafolder+'wikibase/DavidL_bot_pwd.txt', 'r', encoding='utf-8') as pwdfile:
+	awbuserpass = pwdfile.read()
 
 # with open(datafolder+'zoteroapi/zotero_api_key.txt', 'r', encoding='utf-8') as pwdfile:
 # 	zotero_api_key = pwdfile.read()
@@ -11,6 +13,9 @@ PREFIX adp: <http://datuak.ahotsak.eus/prop/direct/>
 PREFIX ap: <http://datuak.ahotsak.eus/prop/>
 PREFIX aps: <http://datuak.ahotsak.eus/prop/statement/>
 PREFIX apq: <http://datuak.ahotsak.eus/prop/qualifier/>
+PREFIX apr: <http://datuak.ahotsak.eus/prop/reference/>
 """
 # Properties with constraint: max. 1 value
-max1props = []
+max1props = [
+"P1"
+]
